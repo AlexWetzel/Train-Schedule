4f571edd140a26fb9f477105364e4f5f811d03ef
AlexWetzel <wetzela@live.com>
2017-09-21 23:07:05 -0400

    changed code to do calculations with moment.js

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ Link: https://alexwetzel.github.io/Train-Schedule.github.io/
 
 A table that uses Firebase to log and display train times.
 
-The website displays a table with information stored in Firebase, and a form that allows users to submit train information. Users can enter the train name, desination, the time between arrivals, and the initial departure time. This informations is stored in Firebase, and then added to the train schedule. The time of the next arrival and the minutes to the next arrival are calculated in Javascript and added to the table.
+The website displays a table with the following information:
+-Train name
+-Destination
+-Period (minutes between arrivals)
+-Next arrival
+-Minutes until the train arrives
+
+The train name, desination, the time between arrivals, and the initial departure time are taken from a form input and stored in an object in Firebase. The script takes all objects in Firebase and sorts them in order by key. As this happens, the next arrival and the minutes to the next train are calculated using Moment.js. After this, all of the information is added to the table.
 
 Note: In the instructions, the time between arrivals is labeled as frequency, but frequency is defined as a number of occurrences over time. The inverse of frequency is the period, time between occurrences, which is what I use.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,89 +8,41 @@ var config = {
   };
 firebase.initializeApp(config);
 
+var now = moment();
+
 var db = firebase.database();
 
-//Converts a time of day into minutes
-function toMinutes(time){
-	var start = moment(time, "HH-mm");
-	var hours = parseInt(start.format("HH"));
-	var minutes = parseInt(start.format("mm"));
-	return minutes + (hours * 60);
-}
+//If the time entered is after the current time, a day is subtracted
+function checkTime(time) {
+	var minutes = moment(now, "HH-mm").diff(time, "minutes");
 
-//Converts minutes to hours in military time
-function toHours(time){
-	
-	//Prevents unintended behavior where the time would be greater than 23:59
-	if (time >= 1440) {
-		time = time % 1440;
-	}
-
-	var hours = Math.floor(time/60);
-	var minutes = time % 60;
-
-	hours = formatTime(hours);
-	minutes = formatTime(minutes);
-
-	return hours + ":" + minutes;
-}
-
-//If the hours or minutes are less than 10, adds a zero to the value
-function formatTime(num) {
-	if(num < 10) {
-		num = "0" + num;
-	}
-	return num;
+	if (minutes < 0) {
+      time = moment(time).subtract(1, "days");
+      minutes = moment(now, "HH-mm").diff(time, "minutes");
+    }
+    return time;
 }
 
 //Orders the database entries in chronological order (by key) and adds them to the web page
 db.ref().orderByKey().on("child_added", function(snapshot) {
 
 	var train = snapshot.val();
-   
- /* Was trying to be neat here, but my code wouldn't work
- ----------------------------------------
-   // var cell = $("<td>");
-   
-   // var name = train.val().name;
-   // var destination = train.val().destination;
-   // var period = train.val().period;
-   // var nextarrival = train.val().nextarrival;
 
-   // var row = $("<tr>")
-   // row.append(cell.text(name));
-   // row.append(cell.text(destination));
-   // row.append(cell.text(period));
-   // row.append(cell.text(nextarrival));
-   //  console.log(name);
-   // $("table").append(row);
-------------------------------------------*/   
-
-	var time = train.departure;
 	var period = parseInt(train.period);
+	var time = train.departure;
+	var time = moment(time, "HH-mm--MM-DD-YYYY")
 
-	var startMin = toMinutes(time);
-
-	var now = moment();
-
-	var currentMin = toMinutes(now);
-
-	//Accounts for the difference in time between days
-	if (currentMin < startMin) {
-		currentMin += 1440;
-	}
-
-	var diff = currentMin - startMin;	
-
-	var rem = diff % period;
-	var nextMin = currentMin + period - rem;
-	var timeLeft = nextMin - currentMin;
-
-	var nextTime = toHours(nextMin);
-
+	
+	var minutes = moment(now, "HH-mm").diff(time, "minutes");
+	//The next arrival time is calculated by getting the remainder of the difference divided by the period
+	var remainder = minutes % period;
+	//subtracting the remainder from the period gives us the time until the next arrival
+    var timeLeft = period - remainder;    
+    nextTime = moment().add(timeLeft, "minutes").format("hh:mm a");
+	
 	$("table").append("<tr><td>" + train.name + "</td><td>" + train.destination + "</td><td>" + train.period + "</td><td>" + nextTime + "</td><td>" + timeLeft + "</td></tr>");
 }, function(errorObject){
-
+	console.log("something went wrong");
 });
 
 //Gets the values from the form and adds them to the database
@@ -102,15 +54,19 @@ $("#enter-button").on( "click", function(event) {
 	var periodIn = $("#period").val().trim();
 	var departureIn = $("#departure").val().trim();
 
-	console.log(nameIn);
+
+	var parsedTime = moment(departureIn, "HH-mm");
+	var checkedTime = checkTime(parsedTime);
+
+	//the date is added to the database, as the calculations take it into account
+	departureIn = moment(checkedTime).format("HH:mm, MM/DD/YYYY")
 	
 	db.ref().push({
 		name: nameIn,
 		destination: destIn,
 		period: periodIn,
-		departure: departureIn
+		departure: departureIn,
 	});
-
 });
 
 
